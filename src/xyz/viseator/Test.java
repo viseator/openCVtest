71d81a909dd3ce3f5ad54c96d8d89e0aab79624c
viseator <viseator@gmail.com>
2016-11-30 10:56:09 +0800

    init

---
+++ src/xyz/viseator/Test.java
@@ -0,0 +1,17 @@
+package xyz.viseator;
+
+import org.opencv.core.Core;
+
+/**
+ * Created by viseator on 2016/11/21.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        final String PATH = "C:/Users/visea/Desktop/test/2.jpg";
+        ProgressPic progressPic;
+        progressPic = new ProgressPic();
+        progressPic.progress(PATH,1);
+
+    }
+}

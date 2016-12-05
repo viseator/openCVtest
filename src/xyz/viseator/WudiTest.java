1fc8c45b93ffc70162e6c3eabf60228714ef6674
viseator <viseator@gmail.com>
2016-12-05 11:30:52 +0800

    Data Store Done not OOP

--- src/xyz/viseator/WudiTest.java
+++ src/xyz/viseator/WudiTest.java
@@ -2,16 +2,37 @@ package xyz.viseator;
 
 import org.opencv.core.Core;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Created by viseator on 2016/11/21.
  */
 public class WudiTest {
     public static void main(String[] args) {
+        ArrayList<ArrayList<BufferedImage>> bufferedImages;
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        final String PATH = "C:/Users/visea/Desktop/test/5.jpg";
+        final int pic = 4;
+        final String PATH = "C:/Users/visea/Desktop/test/" +
+                String.valueOf(pic) + ".jpg";
         ProgressPic progressPic;
         progressPic = new ProgressPic();
-        progressPic.progress(PATH,5);
-
+        bufferedImages = progressPic.progress(PATH, pic);
+        BufferedImage image;
+        for (int cols = 0; cols < bufferedImages.size(); cols++) {
+            for (int character = 0;character<bufferedImages.get(cols).size();character++) {
+                try {
+                    image = bufferedImages.get(cols).get(character);
+                    File file = new File("C:/Users/visea/Desktop/test/java/cut5/" +
+                            String.valueOf(cols) + "_" + String.valueOf(character) + ".jpg");
+                    ImageIO.write(image,"jpg",file);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }

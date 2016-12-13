f319fd74286b9ef9f3670b1912c9debeb646d031
Alex <yifengtang_hust@outlook.com>
2016-12-13 10:52:58 +0800

    Add HeaderOCR

---
+++ src/xyz/viseator/HeaderOCR.java
@@ -0,0 +1,34 @@
+package xyz.viseator;
+
+
+import net.sourceforge.tess4j.ITessAPI;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+/**
+ * Created by Lily on 2016/12/13.
+ * Email: yifengtang@unique.com
+ */
+public class HeaderOCR {
+
+    private OCRHandler handler;
+
+    public HeaderOCR(String dataPath, String language, String dicPath) {
+        handler = new OCRHandler();
+        handler.init(dataPath, dicPath, language, OCRHandler.FILTER_CHI);
+    }
+
+    public HeaderOCR(String dataPath, String dicPath){
+        this(dataPath, "chi_sim", dicPath);
+    }
+
+    public String getText(ArrayList<BufferedImage> images){
+        StringBuilder builder = new StringBuilder();
+        for(BufferedImage image : images){
+            builder.append(handler.getTextFromPic(image, ITessAPI.TessPageSegMode.PSM_SINGLE_LINE));
+        }
+        return builder.toString();
+    }
+
+}
